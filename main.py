048cb975b4c2525f8b8736763be8809f98e06c29
Adriell Marques <adriel.gomes@nepen.org.br>
2024-08-24 13:31:32 -0300

    save repo

---
+++ main.py
@@ -0,0 +1,12 @@
+import matplotlib
+matplotlib.use('TkAgg')
+from runs import KNNRuns,DMCRuns,BayesianGaussianDiscriminantRuns,KmeansQuantRuns, BayesianRejectionRuns,BayesianGaussianMixtureRuns
+
+
+def main():
+    BayesianGaussianMixtureRuns(0)
+    BayesianGaussianMixtureRuns(1)
+    BayesianGaussianMixtureRuns(2)
+
+if __name__ == "__main__":
+    main()
